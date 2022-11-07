7b3caf8807b40d59838a09e791c095812554e3fc
lopez824 <lionelslopez14@gmail.com>
2022-11-07 16:23:27 -0700

    Added 2nd level to demonstrate usability

--- Assets/Scripts/LevelGoal.cs
+++ Assets/Scripts/LevelGoal.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelGoal : MonoBehaviour
 {
     public UIHandler uiHandler;
 
+    public IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene("SnowCast");
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
+        {
             uiHandler.startMessage.SetText("Hooray! You reached the boat!!");
+            StartCoroutine(LoadNextLevel());
+        }
     }
 }
